2d3735c70808aaa8920402f3d53f18a42f6c8b4b
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-05 20:15:47 +0530

    basic layout created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Chat from "./components/Chat/Chat";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Chat */}
+        <Chat />
+      </div>
+    </div>
+  );
 }
 
 export default App;
