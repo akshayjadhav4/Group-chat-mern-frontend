d2afa995013d8cac75efe7b5ed4d8638a908f6ad
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-06 21:30:14 +0530

    getting messages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Chat from "./components/Chat/Chat";
-
+import Pusher from "pusher-js";
+import api from "./api/index";
 function App() {
+  const [messages, setMessages] = useState([]);
+  //loading all messages from db
+  useEffect(() => {
+    api
+      .get("/api/messages/sync")
+      .then((response) => setMessages(response.data));
+  }, []);
+
+  // getting real-time messages
+  useEffect(() => {
+    const pusher = new Pusher(process.env.REACT_APP_PUSHER_ID, {
+      cluster: process.env.REACT_APP_PUSHER_CLUSTER,
+    });
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (newMessages) {
+      setMessages([...messages, newMessages]); //adding new messages to messages
+    });
+
+    return () => {
+      pusher.unbind_all();
+      pusher.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
   return (
     <div className="app">
       <div className="app__body">
