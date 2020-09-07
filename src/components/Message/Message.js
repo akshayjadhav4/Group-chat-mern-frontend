1fb840b9614c81997304c31e5f97925bbb7b0015
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-07 20:18:02 +0530

    sender receiver  difference logic added

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Message.css";
-function Message({ message, isSender }) {
+function Message({ message }) {
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+
   return (
     <div className="message">
-      <p className={`message__body ${isSender && "message__body--sender"}`}>
+      <p
+        className={`message__body ${
+          user.displayName === message.name && "message__body--sender"
+        }`}
+      >
         <span className="message__name">{message.name}</span>
         {message.message}
         <span className="message__timestamp">{message.timeStamp}</span>
